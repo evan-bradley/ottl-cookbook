8c548fb883d242e78858916bea8071cde4878a50
Evan Bradley <11745660+evan-bradley@users.noreply.github.com>
2024-10-14 13:55:49 -0400

    Fix config for code snippets, draft rendering script

---
+++ render.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+directories=(code)
+
+for dir in $directories; do
+    pushd ${dir}
+    
+    mkdir -p output
+
+    files=$(find . -iname '*.mmd' -type f)
+    for file in $files; do
+        docker run --rm -u $(id -u):$(id -g) -v $(pwd):/data minlag/mermaid-cli -c config.json -s 10 -i ${file} -o output/$(basename ${file}).png
+    done
+
+    popd
+done
