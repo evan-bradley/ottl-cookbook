4b12b08bfc117b128f6867583ae8d93168f8cf72
Evan Bradley <11745660+evan-bradley@users.noreply.github.com>
2024-10-31 15:18:30 -0400

    Clear old images when rendering

--- render.sh
+++ render.sh
@@ -8,6 +8,7 @@ for dir in $directories; do
     mkdir -p output
 
     files=$(find . -iname '*.mmd' -type f)
+    rm -f output/*.png
     for file in $files; do
         docker run --rm -u $(id -u):$(id -g) -v $(pwd):/data minlag/mermaid-cli -c config.json -s 10 -i ${file} -o output/$(basename ${file}).png
     done
